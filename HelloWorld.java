fd404996ad1f765495064078743eba3a2f6acc2b
Ander <ander@localhost.localdomain>
2019-04-09 22:23:50 -0300

    Añadido método limpiar()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,15 @@
 class HelloWorld {
 	public static void main (String[] args) {
+		limpiar();
 		System.out.println("Hello World!!!");
 		System.out.println("Goodbye!!!");
+	}
+
+	static void limpiar() {
+		try {
+			new ProcessBuilder("clear").inheritIO().start();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
