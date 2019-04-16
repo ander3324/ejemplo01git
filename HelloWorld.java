de8226670124cdcd5c73ab774468a594855404a5
MatiasEspindola <matiasespindolarp@hotmail.com>
2019-04-16 05:53:29 -0300

    Agregado Division

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 class HelloWorld {
+
 	public static void main (String[] args) {
 		limpiar();
 		System.out.println("Hello World!!!");
@@ -11,5 +12,19 @@ class HelloWorld {
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
+	}
+
+	static double dividir(double n1, double n2){
+	      double res;
+		if(n2 != 0){
+			res = (n1 / n2);
+		}else{
+		             errorDivision();
+		}
+	     return res;
+	}
+
+	static String errorDivision(){
+	    return "No se puede dividir por 0";
 	}
 }
